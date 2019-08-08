1d95b5e0092f87d7add4478403c358315bf3427b
Alexandra Vanhalme <alexandravanhalme@hotmail.com>
2019-08-08 15:53:34 +0200

    add new and create for doses

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,24 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.save
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:ingredient_id, :cocktail_id, :description)
+  end
+end
